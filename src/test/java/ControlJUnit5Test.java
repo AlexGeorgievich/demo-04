f4c73887e2dec737cf66f4abf6160ec3dfd2aa20
AlexGeorgievich <alexhome1@yandex.ru>
2022-02-26 22:09:09 +0300

    Initial commit

---
+++ src/test/java/ControlJUnit5Test.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ControlJUnit5Test {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterAll
+    static void AfterAllTest() {
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void FindingSoftAssertionsTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(withText("more pages")).click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").$(byText("Using JUnit5 extend test class:"));
+
+    }
+}
---
+++ src/test/java/DragAndDrop.java
@@ -0,0 +1,35 @@
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrop {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+    }
+
+    @AfterAll
+    static void AfterAll() {
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void dragAndDropTo() {
+
+        open("/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $$("#column-a").shouldHave(texts("B"));
+        $$("#column-b").shouldHave(texts("A"));
+
+
+    }
+}
+
